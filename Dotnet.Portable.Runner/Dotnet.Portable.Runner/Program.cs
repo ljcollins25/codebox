a8a539a0b48fb5d47d64e0cc39f04b5db90c3b69
Lance Collins <lancec@microsoft.com>
2025-11-01 16:42:05 -0700

    Portable runner.

---
+++ Dotnet.Portable.Runner/Dotnet.Portable.Runner/Program.cs
@@ -0,0 +1,30 @@
+﻿//using Microsoft.NET.HostModel.AppHost;
+
+namespace Dotnet.Portable.Runner;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //HostWriter.CreateAppHost()
+        //Console.WriteLine("Hello, World!");
+
+        StackOverflowTest(default, int.Parse(args[0]));
+    }
+
+
+    private static void CurrentDomain_FirstChanceException(object? sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+    {
+        Console.WriteLine("Handling exception");
+    }
+
+    static void StackOverflowTest(Guid guid, int index)
+    {
+        if (index <= 0)
+        {
+            return;
+        }
+
+        StackOverflowTest(guid, index - 1);
+    }
+}
